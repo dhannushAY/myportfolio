a2796b153926e6412cb87c5271a504207a5978d8
dhannushAY <81463007+dhannushAY@users.noreply.github.com>
2021-11-03 00:00:44 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,110 @@
+export const projects = [
+  {
+    title: "Movie Seat Booking",
+    subtitle: "MERN Stack",
+    description:
+      "A Movie Seat Booking Site built using Js,Html,Css",
+    image: "./Screenshot (85).png",
+    link: "https://primaryluminousembed.barathkumar87.repl.co/",
+  },
+  {
+    title: "E-Commerce",
+    subtitle: "ReactJs",
+    description:
+      "A E-Commerce App using react",
+    image: "./Screenshot (87).png",
+    link: "https://csb-22me3.netlify.app/",
+  },
+  {
+    title: "Rock-Paper-Scissor",
+    subtitle: "Javascript",
+    description:
+      "A Classic game built on Javascript",
+    image: "./Screenshot (89).png",
+    link: "https://rock-paper-scissors-game-by-barath-fsd21a02.netlify.app/",
+  },
+  {
+    title: "Guess My Number",
+    subtitle: "Javascript",
+    description:
+      "This JavaScript guessing game objective is to make the user guess a randomly generated number from 1 to 20",
+    image: "./Screenshot (91).png",
+    link: "https://guess-my-number.barathkumar87.repl.co/",
+  },
+  {
+    title: "Dice Game",
+    subtitle: "Javascript and HTML,CSS",
+    description:
+      "A Simple Dice Game using Javascript",
+    image: "./Screenshot (93).png",
+    link: "https://lonelyfinancialacrobat.barathkumar87.repl.co/",
+  },
+  {
+    title: "Space Quiz",
+    subtitle: "Javascript and HTML,CSS",
+    description:
+      "A Quiz site using JavaScript",
+    image: "./Screenshot (96).png",
+    link: "https://spacequiz.barathkumar87.repl.co/",
+  },
+  {
+    title: "React To-Do(JSX-CSS)",
+    subtitle: "ReactJs",
+    description:
+      "A React App built only by using JSX and CSS",
+    image: "./Screenshot (97).png",
+    link: "https://react-xzcmld.stackblitz.io/",
+  },
+  {
+    title: "Random Quote Generator",
+    subtitle: "Javascript and HTML,CSS",
+    description:
+      "An Application that generates random quotes",
+    image: "./Screenshot (100).png",
+    link: "https://random-quote-a02323.netlify.app/",
+  },
+  {
+    title: "Weather App",
+    subtitle: "ReactJs",
+    description:
+      "A weather app using react",
+    image: "./Screenshot (106).png",
+    link: "https://barathkumar87.github.io/weather-app-react/",
+  },
+  {
+    title: "Expense Calculator",
+    subtitle: "ReactJs",
+    description:
+      "A Expense Calculating app using React",
+    image: "./Screenshot (104).png",
+    link: "https://random-quote-a02323.netlify.app/",
+  },
+];
+
+// export const testimonials = [
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/1.jpg",
+//     name: "Jesse Hicks",
+//     company: "Zoozle",
+//   },
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/94.jpg",
+//     name: "Ruben Alvarez",
+//     company: "Dooble.io",
+//   },
+// ];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "HTML & CSS",
+  "Node",
+  "Python",
+  "Material UI",
+  "Figma & Photoshop",
+  "Ethical Hacking"
+];
